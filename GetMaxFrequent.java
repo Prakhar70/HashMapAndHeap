6fa0ad836b0d199f91401576255ff3ba2a1cd4fe
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-10-01 21:07:08 +0530

    get Max Frequent Character in a string

---
+++ GetMaxFrequent.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class GetMaxFrequent{
+    public char getMaxFrequent(String s){
+        
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(char c:s.toCharArray()){
+            if(hm.containsKey(c)){
+                int val = hm.get(c);
+                hm.put(c, val+1);
+            }
+            else{
+                hm.put(c, 1);
+            }
+        }
+        int max = hm.get(s.charAt(0));
+        char ans= s.charAt(0);
+        for(Character key : hm.keySet()){
+            if(hm.get(key) > max){
+                max = hm.get(key);
+                ans = key;
+            }
+        }
+        return ans;
+
+    }
+}
