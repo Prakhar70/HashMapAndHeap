2eede98985e88dd1ef6c82980a28b750005ad912
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-10-04 21:33:22 +0530

    mergeKSortedArrayList.java

---
+++ MergeKSortedArrayList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class MergeKSortedArrayList {
+    static class Pair {
+        int listIdx;
+        int eleIdx;
+        int ele;
+
+        Pair(int listIdx, int eleIdx, int ele) {
+            this.listIdx = listIdx;
+            this.eleIdx = eleIdx;
+            this.ele = ele;
+        }
+        public int compareTo(Pair o){
+            return this.ele - o.ele;
+        }
+    }
+
+    public static ArrayList<Integer> mergeSortedLists(ArrayList<ArrayList<Integer>> lists) {
+        int k = lists.size();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>();
+        for (int i = 0; i < k; i++) {
+            pq.add(new Pair(i, 0, lists.get(i).get(0)));
+        }
+        while (!pq.isEmpty()) {
+            Pair rPair = pq.remove();
+            ans.add(rPair.ele);
+            rPair.eleIdx++;
+            if (lists.get(rPair.listIdx).size() > rPair.eleIdx)
+                rPair.ele = lists.get(rPair.listIdx).get(rPair.eleIdx);
+        }
+        return ans;
+    }
+}
